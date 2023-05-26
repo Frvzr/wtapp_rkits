fd7b76c11a10b869a862c7a9ecae2675c54334a8
Frvzr <ikoposhilov@yandex.ru>
2023-05-26 14:08:37 +0500

    add store sheet, column reserved and fix logical for remove from store

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 from math import floor
-import xlsxwriter
 import logging
 import pathlib
 from openpyxl.styles import Alignment
+import decimal
 
 DIR = os.getcwd()
 FILE_PATH = f'{DIR}\\required_redress_kit.xlsx'
@@ -71,6 +71,7 @@ def get_data_from_excel(FILE_PATH):
             for quantity_on_store, required_quantity in zip(v["Q-ty on store"], v["Req qty"]):
                 required_redress_kits["series"][-1]["total"].append({"q-ty on store": quantity_on_store, "required": required_quantity})
 
+        # Можно не создавать 3 словаря а сразу вливать в один...
         rk_bom = pd.read_excel(FILE_PATH, sheet_name='Redress kit BOM')
         redress_kit_bom = {"series": []}
         for redress_kit_, redress_kit_items in rk_bom.groupby("Redress Part Number"):
@@ -123,24 +124,36 @@ def merge_store(qty_on_store_data, required_with_items):
         max_collect_redress = {'series': []}
         for a, b in required_with_items.items():
             for i in b:
+                required = i['total'][-1]['required'] 
                 qty_on_store = {'qty_on_store': []}
                 max_collect_items = {'max_collect_items': []}
+                reserved = {'reserved': []}
                 for y in i['consist']:
                     for k, v in qty_on_store_data.items():
                         if y['item'] == k.upper():
                             max_collect_item = floor(int(v) / int(y['qty']))
+                            if int(v) > 0:
+                                req = int(y['qty']) * int(required)
+                                reserv = int(v) - req
+                                if reserv >= 0:
+                                    reserved['reserved'].append({"item": k.upper(), "qty": req})
+                                elif reserv < 0:
+                                    reserved['reserved'].append({"item": k.upper(), "qty": v})
+                            else:
+                                reserved['reserved'].append({"item": k.upper(), "qty": 0})
+
                             max_collect_items['max_collect_items'].append({"item": k.upper(), "qty": max_collect_item})
                             qty_on_store['qty_on_store'].append({'item': k, 'qty': v})
-
-                required = i['total'][-1]['required']        
+                            
+                print(reserved)      
                 res = get_min_data(max_collect_items)
                 if not pd.isna(required) and res > required:
                     res = required
-                if res > 0:    
-                    qty_on_store_data = update_store(qty_on_store_data, i['consist'], res)
+                qty_on_store_data = update_store(qty_on_store_data, i['consist'], res, reserved['reserved'])
                 
-                max_collect_redress['series'].append({'redress_kit':i['redress_kit'], "total": i["total"], "consist": i["consist"], "max_collect_items": max_collect_items["max_collect_items"], "maximum_collect": res, 'qty_on_store': qty_on_store['qty_on_store']})
-        return max_collect_redress
+                max_collect_redress['series'].append({'redress_kit':i['redress_kit'], "total": i["total"], "consist": i["consist"], "max_collect_items": max_collect_items["max_collect_items"], "maximum_collect": res, 'qty_on_store': qty_on_store['qty_on_store'], 'reserved': reserved['reserved']})
+                #print(max_collect_items)
+        return max_collect_redress, qty_on_store_data
     except Exception as e:
         logger.critical(e)
                         
@@ -159,25 +172,40 @@ def get_min_data(data):
         logger.error(f'{data} - {e}')
 
 
-def update_store(qty_on_store_data, required_items, res):
+def update_store(qty_on_store_data, required_items, res, reserved):
+    
     try:
-        for i in required_items:
+        if reserved:
+            need_remove = [dict(i, **j) for i, j in zip(required_items, reserved)]
+        else:
+            need_remove = required_items
+                 
+        for j in need_remove:
             for k, v in qty_on_store_data.items():  
-                if i['item'] == k.upper():
-                    qty_on_store_data[k] = v - (int(i['qty']) * res)
+                if j['item'] == k.upper() and (v - int(j['qty'])) > 0:
+                    qty_on_store_data[k] = v - int(j['qty'])                              
+                elif j['item'] == k.upper() and (v - int(j['qty'])) <= 0:
+                    qty_on_store_data[k] = 0
         return qty_on_store_data
     except Exception as e:
         logger.error(e)
 
 
-def handling_data(data):
+def handling_data(data, updated_store):
+    #print(data)
     out_data = {'Redress Kit':[],
                 'Qty on store': [],
                 'Required': [],
                 'Can collect': [],
                 'Item': [],
                 'Description': [],
-                'Need to order': []}
+                'Need to order': [],
+                'Reserved':[]
+    }
+    
+    store_data = {'Item': [],
+                  'Quantity': []}
+    
     try:
         for i in data['series']:
             required = i['total'][-1]['required']
@@ -190,23 +218,34 @@ def handling_data(data):
             for j in i['consist']:
                 need_qty = j['qty'] * required
                 for a in i['qty_on_store']:
-                    if j['item'].upper() == a['item'].upper() and need_qty > a['qty']:
-                        out_data["Redress Kit"].append(i['redress_kit'])
-                        out_data['Qty on store'].append(i['total'][-1]['q-ty on store'])
-                        out_data['Required'].append(required)
-                        out_data['Can collect'].append(max_collect)
-                        out_data['Item'].append(a['item'])
-                        out_data['Description'].append(j['description'])
-                        out_data['Need to order'].append(need_qty - a['qty']) 
-                    elif required <= max_collect and i['redress_kit'] not in out_data["Redress Kit"]:
-                        out_data["Redress Kit"].append(i['redress_kit'])                        #
-                        out_data['Qty on store'].append(i['total'][-1]['q-ty on store'])        # NOT DRY - надо переделать
-                        out_data['Required'].append(required)                                   #
-                        out_data['Can collect'].append(max_collect)                             #
-                        out_data['Item'].append('N/a')
-                        out_data['Description'].append('N/a')
-                        out_data['Need to order'].append('N/a')
-        return out_data
+                    for b in i['reserved']:
+                        if j['item'].upper() == a['item'].upper() and j['item'].upper() == b['item'].upper(): #and need_qty > a['qty']:
+                            out_data["Redress Kit"].append(i['redress_kit'])
+                            out_data['Qty on store'].append(i['total'][-1]['q-ty on store'])
+                            out_data['Required'].append(required)
+                            out_data['Can collect'].append(max_collect)
+                            out_data['Item'].append(a['item'])
+                            out_data['Description'].append(j['description'])
+                            if (need_qty - a['qty']) > 0:
+                                out_data['Need to order'].append(need_qty - a['qty'])
+                            else:
+                                out_data['Need to order'].append(0)
+                            out_data['Reserved'].append(b['qty']) 
+                        # elif required <= max_collect and i['redress_kit'] not in out_data["Redress Kit"]:
+                        #     out_data["Redress Kit"].append(i['redress_kit'])                        #
+                        #     out_data['Qty on store'].append(i['total'][-1]['q-ty on store'])        # NOT DRY - надо переделать
+                        #     out_data['Required'].append(required)                                   #
+                        #     out_data['Can collect'].append(max_collect)                             #
+                        #     out_data['Item'].append('N/a')
+                        #     out_data['Description'].append('N/a')
+                        #     out_data['Need to order'].append(0)
+            print(out_data)
+        
+        for k, v in updated_store.items():
+            store_data['Item'].append(k)
+            store_data['Quantity'].append(v)              
+        
+        return out_data, store_data
     
     except Exception as e:
         logger.error(e)
@@ -221,14 +260,18 @@ def get_text_color(val):
 def get_center_text(val):
     return 'text-align: center'
         
-def output_data(all_data):
+def output_data(all_data, store_data):
+    #print(all_data)
     try:
         out_path = pathlib.Path('can_collect_redress_kits.xlsx')
         with pd.ExcelWriter('can_collect_redress_kits.xlsx', engine="openpyxl", mode='a', if_sheet_exists="replace") if out_path.exists() else pd.ExcelWriter('can_collect_redress_kits.xlsx', engine="openpyxl", mode='w') as wb:
             df = pd.DataFrame(all_data)
+            df_store = pd.DataFrame(store_data)
             SHEETNAME = 'Collect Redress Kit'
-            # мешанина по стилям
+            SHEETNAME_STORE = 'Store'
+  
             df.style.applymap_index(bg_header, axis=1).applymap_index(get_text_color, axis=1).applymap_index(get_center_text, axis=1).set_properties(**{'text-align': 'center'}).to_excel(wb, sheet_name=SHEETNAME, index=False)
+            df_store.to_excel(wb, sheet_name=SHEETNAME_STORE, index=False)
             ws = wb.sheets[SHEETNAME]
             ws.auto_filter.ref='a:g'
             ws.column_dimensions['A'].width = 13
@@ -246,7 +289,7 @@ def output_data(all_data):
             ws['E1'].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
             ws['F1'].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
             ws['G1'].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
-            
+            ws['H1'].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
             
             logger.info('End programm')
     except Exception as e:
@@ -256,10 +299,11 @@ def output_data(all_data):
 def main():
     required_redress_kits, redress_kit_bom, qty_on_store_data = get_data_from_excel(FILE_PATH)
     required_with_items = merge_consist(required_redress_kits, redress_kit_bom)
-    raw_data = merge_store(qty_on_store_data, required_with_items)
-    all_data = handling_data(raw_data)
-    output_data(all_data)
+    raw_data, updated_store = merge_store(qty_on_store_data, required_with_items)
+    all_data, store_data = handling_data(raw_data, updated_store)
+    output_data(all_data, store_data)
     
     
 if __name__ == '__main__':
     main()
+    
