0b10dce458a4eb211f48b6ced971b9d86b13a745
Ivan Koposhilov <ikoposhilov@yandex.ru>
2023-11-28 22:20:17 +0500

    test.py added for tests

---
+++ test.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+from math import floor
+
+
+DIR = os.getcwd()
+FILE_PATH_TEST = f'{DIR}\\TPCI - Invetory Control 2023_20231128.xlsx'
+
+
+def get_data_from_excel(FILE_PATH):
+    """
+    Функция получает данные с файла
+
+    Args:
+        FILE_PATH (_str_): Путь к файлу с обрабатываемыми данными
+
+    Returns:
+        (_dict_): Функция возвращает данные с 3-х заранее известных страниц файла
+    """
+
+    df = pd.read_excel(FILE_PATH, sheet_name='Stock 2023')
+    dataset = {'Main':{}, 'Ru Ops': {}}
+    total = {}
+    agg_func_math = {'QTY':['sum']}
+    data = df.groupby(['Part Number', 'Stock', ]).agg(agg_func_math).to_dict()
+    for k, v in data.items():
+        for i, j in v.items():
+            if i[1] == 'Main':
+                dataset['Main'][i[0]] = j
+            else:
+                dataset['Ru Ops'][i[0]] = j
+    #print(dataset)
+    for k, v in dataset.items():
+        if k in total:
+            total[k] += v
+        else:
+            total[k] = v
+    #print(total)
+    
+    serial_n = df.groupby(['Part Number', 'Stock', 'SN']).agg(agg_func_math).to_dict()
+    print(serial_n)
+   
+ 
+def main():
+    test_data = get_data_from_excel(FILE_PATH_TEST)
+    print(test_data)
+
+
+if __name__ == '__main__':
+    main()
+    
