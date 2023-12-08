b7b473ba78298ef94757b72aec001bd52b574220
Frvzr <ikoposhilov@yandex.ru>
2023-12-08 16:35:18 +0500

    logger has been added

--- new.py
+++ new.py
@@ -1,73 +1,97 @@
 import os
 import pandas as pd
 from math import floor
-import logging
 import pathlib
 from openpyxl.styles import Alignment
+from logger import get_logger
+
+logger = get_logger(__name__)
 
 DIR = os.getcwd()
-FILE_PATH = f'{DIR}\\required_redress_kit.xlsx'
+INPUT_FILE = 'required_redress_kit.xlsx'
+FILE_PATH = f'{DIR}\\{INPUT_FILE}'
 SHEETNAME_INPUT_STOCK = 'Stock 2023'
 SHEETNAME_INPUT_REQUIRED = 'Required redress kits'
 SHEETNAME_INPUT_BOM = 'Redress kit BOM'
 
 OUTPUT_FILE = "can_collect_redress_kits.xlsx"
 SHEETNAME_OUTPUT = 'Collect Redress Kit'
-SHEETNAME_STORE_OUTPUT = 'Updated store'
+SHEETNAME_STORE_OUTPUT = 'Updated stocks'
 
 
 def get_data_from_excel_stock(path):
-    stock_dataframe = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_STOCK)
-    
-    agg_qty_sum = {'QTY':['sum']}
-    data_by_stock = stock_dataframe.groupby(['Part Number', 'Stock']).agg(agg_qty_sum).to_dict()
-    
-    items_by_stock = {'Main': {}, 'Ru Ops': {}}
-    for data in data_by_stock.values():
-        for data_from_stock, qty in data.items():
-            if data_from_stock[1].lower() == 'main':
-                items_by_stock['Main'][data_from_stock[0]] = qty
-            else:
-                 items_by_stock['Ru Ops'][data_from_stock[0]] = qty
+    try:
+        logger.info('Start programm')
+        stock_dataframe = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_STOCK)
+        
+        agg_qty_sum = {'QTY':['sum']}
+        data_by_stock = stock_dataframe.groupby(['Part Number', 'Stock']).agg(agg_qty_sum).to_dict()
+        
+        items_by_stock = {'Main': {}, 'Ru Ops': {}}
+        for data in data_by_stock.values():
+            for data_from_stock, qty in data.items():
+                if data_from_stock[1].lower() == 'main':
+                    items_by_stock['Main'][data_from_stock[0]] = qty
+                else:
+                    items_by_stock['Ru Ops'][data_from_stock[0]] = qty
 
-    total = {k: items_by_stock['Main'].get(k, 0) + items_by_stock['Ru Ops'].get(k, 0) for k in set(items_by_stock['Main']) | set(items_by_stock['Ru Ops'])}
+        total = {k: items_by_stock['Main'].get(k, 0) + items_by_stock['Ru Ops'].get(k, 0) for k in set(items_by_stock['Main']) | set(items_by_stock['Ru Ops'])}
 
-    serial_items = stock_dataframe.groupby(['Part Number', 'Stock', 'SN']).agg(agg_qty_sum).to_dict()
-    actual_serial_items = {}
-    for items_sn in serial_items.values():
-        for item_sn, qty_sn in items_sn.items():
-            if qty_sn == 0:
-                continue
-            else:
-                actual_serial_items[item_sn] = qty_sn
-    print(f"Data from {SHEETNAME_INPUT_STOCK} uploaded")
-    return items_by_stock, total, actual_serial_items
+        serial_items = stock_dataframe.groupby(['Part Number', 'Stock', 'SN']).agg(agg_qty_sum).to_dict()
+        actual_serial_items = {}
+        for items_sn in serial_items.values():
+            for item_sn, qty_sn in items_sn.items():
+                if qty_sn == 0:
+                    continue
+                else:
+                    actual_serial_items[item_sn] = qty_sn
+        print(f"Data from {SHEETNAME_INPUT_STOCK} uploaded")
+        return items_by_stock, total, actual_serial_items
+    except ValueError as ve:
+        logger.critical(f"{ve} in the {INPUT_FILE}")
+    except KeyError as ke:
+        logger.critical(f"Please change the title of column back to recommended {ke}")
+    except Exception as e:
+        logger.critical(e)
+
 
 
 def get_data_from_excel_required_redress(path):
-    redress = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_REQUIRED)
-    required_redress_kits = {"Required redress kit": []}
-    for redress_kit, req_qty in redress.groupby("Redress kit", sort=False):
-        required_redress_kits["Required redress kit"].append({"redress_kit": redress_kit.upper(), "total": []})
-        for quantity_on_store, required_quantity in zip(req_qty["Q-ty on store"], req_qty["Req qty"]):
-            required_redress_kits["Required redress kit"][-1]["total"].append({"q-ty on store": quantity_on_store, "required": required_quantity})
-    print(f"Data from {SHEETNAME_INPUT_REQUIRED} uploaded")
-    return required_redress_kits
-    
+    try:
+        redress = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_REQUIRED)
+        required_redress_kits = {"Required redress kit": []}
+        for redress_kit, req_qty in redress.groupby("Redress kit", sort=False):
+            required_redress_kits["Required redress kit"].append({"redress_kit": redress_kit.upper(), "total": []})
+            for quantity_on_store, required_quantity in zip(req_qty["Q-ty on store"], req_qty["Req qty"]):
+                required_redress_kits["Required redress kit"][-1]["total"].append({"q-ty on store": quantity_on_store, "required": required_quantity})
+        print(f"Data from {SHEETNAME_INPUT_REQUIRED} uploaded")
+        return required_redress_kits
+    except ValueError as ve:
+        logger.critical(f"{ve} in the {INPUT_FILE}")
+    except KeyError as ke:
+        logger.critical(f"Please change the title of column back to recommended {ke}")
+    except Exception as e:
+        logger.critical(e)
     
 def get_data_from_excel_redress_kits_bom(path):
-    rk_bom = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_BOM)
-    redress_kit_bom = {"redress kit consist": []}
-    for redress_kit_, redress_kit_items in rk_bom.groupby("Redress Part Number"):
-        redress_kit_bom["redress kit consist"].append({"redress kit": redress_kit_.upper(), "consist": []})
-        for item, qty, desc in zip(redress_kit_items["Item Part Number"], redress_kit_items["Quantity pr."], redress_kit_items['Description']):
-            if qty == 0 or qty == '0':
-                continue
-            else:
-                redress_kit_bom["redress kit consist"][-1]["consist"].append({'item': item, 'description': desc, 'qty': qty})
-    print(f"Data from {SHEETNAME_INPUT_BOM} uploaded")
-    return redress_kit_bom
-
+    try:
+        rk_bom = pd.read_excel(path, sheet_name=SHEETNAME_INPUT_BOM)
+        redress_kit_bom = {"redress kit consist": []}
+        for redress_kit_, redress_kit_items in rk_bom.groupby("Redress Part Number"):
+            redress_kit_bom["redress kit consist"].append({"redress kit": redress_kit_.upper(), "consist": []})
+            for item, qty, desc in zip(redress_kit_items["Item Part Number"], redress_kit_items["Quantity pr."], redress_kit_items['Description']):
+                if qty == 0 or qty == '0':
+                    continue
+                else:
+                    redress_kit_bom["redress kit consist"][-1]["consist"].append({'item': item, 'description': desc, 'qty': qty})
+        print(f"Data from {SHEETNAME_INPUT_BOM} uploaded")
+        return redress_kit_bom
+    except ValueError as ve:
+        logger.critical(f"{ve} in the {INPUT_FILE}")
+    except KeyError as ke:
+        logger.critical(f"Please change the title of column back to recommended {ke}")
+    except Exception as e:
+        logger.critical(e)
 
 def merge_consist(required_redress_kits, redress_kit_bom):
     required_with_items = {'Items for redres kits': []}
@@ -75,7 +99,7 @@ def merge_consist(required_redress_kits, redress_kit_bom):
         for consist in redress_kit_bom["redress kit consist"]:
             if consist["redress kit"] == req_kit['redress_kit']:
                 required_with_items['Items for redres kits'].append({'redress_kit': consist['redress kit'], "total": req_kit["total"], "consist": consist["consist"]})
-    print("Merging required redress kits and BOM was completed")
+    print("Merging required redress kits and BOM has been completed")
     return required_with_items
 
 
@@ -111,7 +135,7 @@ def merge_store(qty_on_store_data, required_with_items, serial_items):
             reserved = get_reserved(res, item, qty_on_store_data)
         qty_on_store_data = update_store(qty_on_store_data, reserved['reserved'])    
         max_collect_redress['maximum collect rkits'].append({'redress_kit':item['redress_kit'], "total": item["total"], "consist": item["consist"], "max_collect_items": max_collect_items["max_collect_items"], "maximum_collect": res, 'qty_on_store': qty_on_store['qty_on_store'], 'reserved': reserved['reserved'], 'serial': serial['serial_items']})
-    
+    print("Merging data with stock has been completed")
     return max_collect_redress, qty_on_store_data
 
 
@@ -231,6 +255,7 @@ def data_handling_items(data, items_by_stock):
                                 out_data['Reserved'].append(b['qty'])
                                 out_data['Main'].append(item_from_main)
                                 out_data['Ru Ops'].append(item_from_ruops)
+    print("The data has been collected")
     return out_data
 
 
@@ -238,7 +263,8 @@ def data_handling_store(updated_store):
     store_data = {'Item': [], 'Quantity': []}
     for item_pn, qty in updated_store.items():
         store_data['Item'].append(item_pn)
-        store_data['Quantity'].append(qty)              
+        store_data['Quantity'].append(qty) 
+    print("The stock has been updated")             
     return store_data
 
 
@@ -254,19 +280,15 @@ def get_text_color(val):
 def get_center_text(val):
     return 'text-align: center'
 
-def create_sheet(workbook, sheet_name):
-    ws = workbook.sheets[sheet_name]
-    ws.auto_filter.ref='a:l'
-    
     
 def format_sheets(col_dims, ws):
     for col_name, col_width in col_dims.items():
         col_name_store_num = col_name + '1'
         ws.column_dimensions[col_name].width = col_width
         ws[col_name_store_num].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
-
         
 def output_data(all_data, store_data):
+    
     out_path = pathlib.Path(OUTPUT_FILE)
     with pd.ExcelWriter(OUTPUT_FILE, engine="openpyxl", mode='a', if_sheet_exists="replace") if out_path.exists() else pd.ExcelWriter(OUTPUT_FILE, engine="openpyxl", mode='w') as wb:
         df = pd.DataFrame(all_data)
@@ -275,7 +297,7 @@ def output_data(all_data, store_data):
         df.style.applymap_index(bg_header, axis=1).applymap_index(get_text_color, axis=1).applymap_index(get_center_text, axis=1).set_properties(**{'text-align': 'center'}).to_excel(wb, sheet_name=SHEETNAME_OUTPUT, index=False)
         df_store.to_excel(wb, sheet_name=SHEETNAME_STORE_OUTPUT, index=False)
         df_store.style.applymap_index(bg_header, axis=1).applymap_index(get_text_color, axis=1).applymap_index(get_center_text, axis=1).set_properties(**{'text-align': 'center'}).to_excel(wb, sheet_name=SHEETNAME_STORE_OUTPUT, index=False)
-            
+        
         ws = wb.sheets[SHEETNAME_OUTPUT]
         ws.auto_filter.ref='a:l' 
         
@@ -288,7 +310,8 @@ def output_data(all_data, store_data):
         ws_store.auto_filter.ref='a:b'
         
         format_sheets(col_dims_store, ws_store)
-                   
+    print(f"The file {OUTPUT_FILE} has been created or updated")
+    logger.info('Data collection and calculation completed')           
             
 def main():
     try:
@@ -299,11 +322,13 @@ def main():
         data, updated_store = merge_store(total, required_with_items, serial_items)
         all_data = data_handling_items(data, items_by_stock)
         store_data = data_handling_store(updated_store)
-        
         output_data(all_data, store_data)
     except PermissionError:
         print(f"Please close file: {OUTPUT_FILE}")
-    
+    except FileNotFoundError as fnf:
+        logger.critical(f"File not found: {INPUT_FILE}")
+    except Exception as e:
+        logger.critical(e)
 
 if __name__ == '__main__':
     main()
